e45c5bd6a5d3c85758a667b9d80206b5e2abae60
Juan Sebastian Sanchez L <js.sanchezl12@uniandes.edu.co>
2023-11-02 00:14:25 -0500

    Update unit tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { sumar, resta, multiplicar, dividir, add, raiz, elevadoal2 } = require("../src/index");
+const { sumar, resta, multiplicar, dividir, add, raiz, elevadoal2, test, test_2, getIP, test_3} = require("../src/index");
 
 describe("Calculadora", function () {
     
@@ -20,6 +20,10 @@ describe("Calculadora", function () {
 
     it("debería lanzar un error al dividir por cero", function () {
         expect(() => dividir(5, 0)).to.throw("Division by zero");
+    });
+
+    it("debería dividir", function () {
+        expect(dividir(4, 2)).to.equal(2);
     });
 
     it("debería sumar números en una cadena separados por comas", function () {
@@ -52,5 +56,67 @@ describe("Calculadora", function () {
         expect(elevadoal2(5, 3)).to.equal(4);
         expect(elevadoal2(10, 3)).to.equal(49);
         expect(elevadoal2(2, 5)).to.equal(9);
+    });
+
+    it("debería convertir 'start' a 'START'", function () {
+        expect(test("start")).to.equal("START");
+    });
+
+    it("debería convertir 'example' a 'EXAMP'", function () {
+        expect(test("example")).to.equal("EXAMP");
+    });
+
+    it("debería manejar un valor nulo y convertir 'start' a 'START'", function () {
+        expect(test(null)).to.equal("START");
+    });
+
+    it("debería manejar un valor indefinido y convertir 'start' a 'START'", function () {
+        expect(test(undefined)).to.equal("START");
+    });
+
+    it("debería tener una cadena START cuando no se proporciona entrada", function () {
+        expect(test()).to.equal("START");
+    });
+});
+
+describe("Función test_2", function () {
+    it("debería retornar '6doSomething' para par = -1", function () {
+        expect(test_2(-1)).to.equal("6doSomething");
+    });
+
+    it("debería retornar '6error' para par = -2", function () {
+        expect(test_2(-2)).to.equal("6error");
+    });
+
+    it("debería retornar '1error' para otros valores de par", function () {
+        expect(test_2(2)).to.equal("6error");
+    });
+});
+
+describe("Función getIP", function () {
+
+    it("debería retornar el valor de la variable de entorno IP_ADDRESS si está definida", function () {
+        process.env.IP_ADDRESS = "192.168.0.1";
+
+        expect(getIP()).to.equal("192.168.0.1");
+    });
+
+    it("debería retornar 'local' si IP_ADDRESS no está definida", function () {
+        process.env.IP_ADDRESS = undefined;
+
+        expect(getIP()).to.equal("undefined");
+    });
+});
+
+
+describe("Función test_3", function () {
+    it("debería retornar un array de expresiones regulares", function () {
+        const patterns = test_3();
+        expect(patterns).to.be.an("array");
+        expect(patterns).to.have.lengthOf(2);
+        expect(patterns[0]).to.be.an.instanceOf(RegExp);
+        expect(patterns[1]).to.be.an.instanceOf(RegExp);
+        expect(patterns[0].test(" ")).to.be.true;
+        expect(patterns[1].test(" ")).to.be.true;
     });
 });
