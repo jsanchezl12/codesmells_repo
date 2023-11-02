dc525a47c49c5eb50314df019685f46484f1a054
Juan Sebastian Sanchez L <js.sanchezl12@uniandes.edu.co>
2023-11-01 21:56:05 -0500

    update files and sonar properties and pipeline

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { sumar, resta, multiplicar, dividir } = require("./index");
+const { sumar, resta, multiplicar, dividir } = require("../src/index");
 
 describe("Calculadora", function () {
     it("debería sumar dos números correctamente", function () {
@@ -18,7 +18,7 @@ describe("Calculadora", function () {
         expect(dividir(10, 2)).to.equal(5);
     });
 
-    
+
     it("debería lanzar un error al dividir por cero", function () {
         expect(() => dividir(5, 0)).to.throw("Division by zero"); // Code Smell: No se manejan errores correctamente
     });
