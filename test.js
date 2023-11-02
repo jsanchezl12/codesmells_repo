8ecfa821395253fe1179d75aed6c564c67af91b7
Juan Sebastian Sanchez L <js.sanchezl12@uniandes.edu.co>
2023-11-01 20:29:51 -0500

    update test.js

--- test.js
+++ test.js
@@ -21,4 +21,5 @@ describe("Calculadora", function () {
     it("debería lanzar un error al dividir por cero", function () {
         expect(() => dividir(5, 0)).to.throw("Division by zero"); // Code Smell: No se manejan errores correctamente
     });
+    
 });
