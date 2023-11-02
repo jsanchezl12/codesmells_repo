45b3860703569e685f3fd49ac816f4397ae2c0e2
Juan Sebastian Sanchez L <js.sanchezl12@uniandes.edu.co>
2023-11-01 22:30:18 -0500

    Update codesmells

--- src/index.js
+++ src/index.js
@@ -41,8 +41,47 @@ function add(numbers) {
             }
         }
     }
-    
+
     return result;
 }
 
-module.exports = { sumar, resta, multiplicar, dividir, add};
+function test() {
+    let a = "start";
+    let b;
+    for (let i = 0; i < 5; i++) {
+        b += a[i].toUpperCase();
+    }
+}
+
+function test_2() {
+    var color = "blue";
+    var size = 4;
+    i = 1;
+    let array = [1, 2, 3];
+
+    for (j = 0; j < array.length; j++) {
+        i = i * array[j];
+    }
+
+    param = 1;
+
+    switch (param) {
+        case 0:
+            console.log("doSomething");
+            break;
+        default:
+            error();
+            break;
+        case 1:
+            console.log("doSomethingElse");
+            break;
+    }
+}
+
+function foo(param) {
+    if (param && param.value) {
+        bar(param.value);
+    }
+}
+
+module.exports = { sumar, resta, multiplicar, dividir, add, test, test_2, foo};
